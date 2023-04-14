08452610c479850b2c6aea593d3962a9e8438f62
Benjamin Bolte <ben@bolte.cc>
2023-04-13 21:13:33 -0400

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,7 @@ def pytest_runtest_setup(item: Function) -> None:
 
 
 def pytest_collection_modifyitems(items: list[Function]) -> None:
-    items.sort(key=lambda x: x.get_closest_marker("slow") is not None, reverse=True)
+    items.sort(key=lambda x: x.get_closest_marker("slow") is not None)
 
 
 def pytest_generate_tests(metafunc: Metafunc) -> None:
